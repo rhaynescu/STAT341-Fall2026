d0269c63b8a067e2ade36be87d2241a0cedd423f
rhaynescu <rhaynes@claflin.edu>
2026-08-18 16:14:06 -0400

    Add files via upload
    First in-class assignment

---
+++ Descriptive-Measures.R
@@ -0,0 +1,18 @@
+# Name: Rodrick Haynes, Date: Aug 18, 2026, Purpose: To test functions of descriptive statistics
+
+# Create a variable called variable1 and store 5 numbers from 1-5.
+
+variable1 <- c(1,2,3,4,5)
+
+# Create mean of variable1
+
+mean(variable1)
+[1] 3
+
+# Calculate standard division
+sd(variable1)
+[1] 1.581139
+
+# Calculate median of variable1
+median(variable1)
+[1] 3
