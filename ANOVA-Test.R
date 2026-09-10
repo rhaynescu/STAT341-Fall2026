5f67937344825053a5b8db7d90ca25fedcf97f41
rhaynescu <rhaynes@claflin.edu>
2026-09-10 16:16:10 -0400

    Add files via upload

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -15,3 +15,16 @@
 	There is no significant difference between type 1 and the other types.
 
 # Plot the dataset using library ggplot2
+	ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+# Apply ANOVA function on dependent variable, time, and independent variable, poison type
+
+	anova_one_way <- aov(time~poison, data = df) 
+	
+	summary(anova_one_way)
+	
+	 Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
